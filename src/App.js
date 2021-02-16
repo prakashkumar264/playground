b2724c874b22845b78079447d6638ee6a0c0e6a5
PrakashKumar <kumar.prak@northeastern.edu>
2021-02-15 21:46:03 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -5,18 +5,30 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
+
+
+
+
+       <p class="makeinbold" >Prakash Kumar</p>
+       <p class="makeinbold" >Prakash Kumar</p>
+       <p class="makeinbold" >Prakash Kumar</p>
+      <p class="makeinbold" >Prakash Kumar</p>
+
+
+       
+       <p>
+         Prakash Kumar
+       </p>
+
+       
+       <p>
+         Prakash Kumar
+       </p>
+       
+       <p>
+         Prakash Kumar
+       </p>
       </header>
     </div>
   );
