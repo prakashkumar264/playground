b3eec1d7d12da7bd61efcee81e54834205a8e1a0
PrakashKumar <kumar.prak@northeastern.edu>
2021-02-18 16:06:32 -0500

    link to github added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Link,
+  Route,
+  Switch,
+} from 'react-router-dom';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-
-       
+      <header className="App-header"> 
        <p class="makeinbold" >Prakash Kumar</p>
-       <p class="makeinbold" >Prakash Kumar</p>
-
-       
-     
+       <Router>
+             <Link to={{ pathname: "https://github.com/prakashkumar264/playground" }} target="_blank" >Github</Link>
+       </Router>
 
       </header>
     </div>
