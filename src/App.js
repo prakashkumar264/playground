3347b392792fdb51b98309d68087392ff81b184a
PrakashKumar <kumar.prak@northeastern.edu>
2021-02-18 19:42:37 -0500

    nav test

--- src/App.js
+++ src/App.js
@@ -2,39 +2,23 @@ import './App.css';
 
 function App() {
 
-
-  let passedinImage="neu.jpg"
-
-  let queryString = `https://mydomain.com/images/${passedinImage}`
-
   return (
-    <div >
+
+    <div>
+ <div className="nav">
     <header>
-         <a href="https://www.wynisco.com/"  target="_blank" rel="noopener noreferrer"><img src="https://cdn.logo.com/hotlink-ok/logo-social.png" height="50px" width="100px"/></a>  
-      <br />
-     <img src={queryString} />
-      <h3>Useful Links</h3>
+
       <ul>
-      <div class="hoverRed">
-        <li>Set up call with instructor : <a href="https://calendly.com/wynisco/isa-information-session?month=2021-02" target="_blank" rel="noopener noreferrer">Here</a> or email sachin@wynisco.com</li>
-        </div>
-        <li>Apply for the program : <a href="https://www.wynisco.com/apply.html" target="_blank">Here</a></li>
-        <li>Website : <a href="https://www.wynisco.com/bootcamp.html" target="_blank">Here</a></li>
+        <li>menu</li>
+        <li>contact</li>
       </ul>
 
-<dvi class="backdemo">
-
-</dvi>
-      <h2>Core Topics</h2>
-      <h2>Welcome to the course</h2>
-      <h4>Application Architecture</h4>
-
-
     </header>
-
+    </div>
 
 
     </div>
+   
   );
 }
 
