ceb16114fc1fb79fb8f4755444280f23e225487e
PrakashKumar <kumar.prak@northeastern.edu>
2021-02-21 12:38:28 -0500

    exercise three completed

--- src/exercisethree.js
+++ src/exercisethree.js
@@ -5,8 +5,27 @@ import React, { Component } from "react";
 class Exercisethree extends Component {
     render() {
       return (
-        <div >
-                      
+        <div class="borderForFlex">
+        <div class="row headerFirst" >
+          <p style={{margin:'auto'}}><em>Header</em></p>
+        </div>
+
+        <div class="row hero" >
+          <p style={{margin:'auto'}} ><em>Hero</em></p>
+        </div>
+
+        <div class="flex-container">
+          <div class="flex-item-left">
+            <p style={{margin:'auto'}} ><em>Content</em></p>
+          </div>
+          <div class="flex-item-right">
+            <p style={{margin:'auto'}} ><em>Sidebar</em></p>
+          </div>
+        </div>
+        <div class="row footer" >
+          <p style={{margin:'auto'}} ><em>Footer</em></p>
+        </div>
+
         </div>
       );
     }
