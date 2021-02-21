3ec8e484f8231c690d3d737949f679a2861a100d
PrakashKumar <kumar.prak@northeastern.edu>
2021-02-20 21:18:44 -0500

    friday commit 2/19

--- src/App.js
+++ src/App.js
@@ -1,29 +1,92 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+const myObject = {};
+
 function App() {
+
+ /*  const myString="prakash";
+  const myNumber= 19;
+  const myBoolean = true ;
+
+  const arr=[1,2,3];
+  console.log(arr, 'arr');
+  arr.push(4);
+  console.log(arr, 'arr');
+
+  const obj = {a:1}
+  console.log(obj.a, 'obj.a');
+  console.log(obj['a']);
+  obj.a=2;
+  console.log(obj, 'new obj');
+
+  const objct={c:1};
+  const copiedobjct=objct;
+  const correctlycopiedobjct={...objct};
+
+  console.log(objct, 'org');
+  console.log(copiedobjct, 'copied');
+  objct.c =2;
+  console.log(objct, 'org');
+  console.log(copiedobjct, 'copied');
+  console.log(correctlycopiedobjct, 'correctly copied')
+
+  const arrobject = [1,2,3,4];
+  const copiedarrobject= arrobject;
+  const correctlyarrobject = {...arrobject};
+
+
+  let prim = 1;
+  let copiedprim = prim;
+  console.log(prim, 'org prim');
+  console.log(copiedprim, 'copied prim');
+
+  prim = 10;
+  console.log(prim, 'org prim');
+  console.log(copiedprim, 'copied prim');
+
+  const myObject ={
+    firstName:"prakash",
+    lastName:"kumar",
+    isStudent:true,
+    address:{
+      street:"1185 Bolyston Street",
+      zipcode:"02215",
+      city:"Boston",
+      state:{
+        mainland:true,
+        outsideState:"MA"
+      }
+    }
+  }
+
+  console.log(myObject, 'myObject');
+
+  console.log(myObject, 'myObject');
+
+  const myArray=["prakash", 20, true, [1,2,3,4,5]]
+ */
+
+
+/*   
+  function concatTwoValues(arg1, arg2){
+    return arg1.concat(arg2);
+  }
+  console.log(concatTwoValues('Prakash', 'Kumar'), '<- Concated Data');
+
+  const arrowDemo = (...args) =>  args[0] * args[1] / .1 ;
+  console.log(arrowDemo(2, Math.PI), '<- Arrow data'); */
+
+  const locateCharacter = (...args) =>  ;
+  console.log(locateCharacter(), 'location');
+
   return (
     <div className="App">
       <header className="App-header">
 
        
        <p class="makeinbold" >Prakash Kumar</p>
-       <p class="makeinbold" >Prakash Kumar</p>
-       <p class="makeinbold" >Prakash Kumar</p>
-       <p class="makeinbold" >Prakash Kumar</p>
        
-       <p>
-         Prakash Kumar
-       </p>
-
-       
-       <p>
-         Prakash Kumar
-       </p>
-       
-       <p>
-         Prakash Kumar
-       </p>
 
       </header>
     </div>
