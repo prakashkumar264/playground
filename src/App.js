8fb0cb8b38263778953cd6692c8414fb16572641
PrakashKumar <kumar.prak@northeastern.edu>
2021-02-21 13:36:39 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -23,6 +23,7 @@ function App() {
         <div className="navbar">
           <ul>
           <li><Link to="/">Home</Link></li>
+          <li>Test</li>
            <li><Link to={{ pathname: "https://github.com/prakashkumar264/playground" }} target="_blank" >Github</Link></li>
            <li><Link to="/Exerciseone">Excercise One</Link></li>
            <li><Link to="/Exercisetwo">Excercise Two</Link></li>
@@ -39,6 +40,8 @@ function App() {
          <Route path="/Exercisefive" component={Exercisefive}/>
  
        </Router>
+
+
 
       
        </div>
