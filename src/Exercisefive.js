ad1b817153560189a982481ca9fdae9540082c34
PrakashKumar <kumar.prak@northeastern.edu>
2021-02-22 15:56:38 -0500

    buttons created

--- src/Exercisefive.js
+++ src/Exercisefive.js
@@ -3,8 +3,24 @@ import React from "react";
 
 function Exercisefive()  {   
       return (
-        <div >
-                      
+        <div className="stockBackground">
+            <label><b>Current Status </b>: Not Started </label>
+            <div className="clockContainer">
+              <div style={{margin:'auto'}}>
+                    <button className="counterBtn">START</button>
+              </div>
+              <div style={{margin:'auto'}}>
+                    <button className="counterBtn">RESET</button>
+              </div>
+              <div style={{margin:'auto'}}>
+                <label>Speed</label>
+                <div className="buttons">
+                    <button className="speedBtn">0.5PX</button>
+                    <button className="speedBtn">5PX</button>
+                    <button className="speedBtn">10PX</button>
+                </div>
+              </div>
+            </div>
         </div>
       );
     
