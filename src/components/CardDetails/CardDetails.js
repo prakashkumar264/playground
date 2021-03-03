7b0c418511f3445a118a598e7d2203b8e68dafe1
PrakashKumar <kumar.prak@northeastern.edu>
2021-03-03 18:43:52 -0500

    Form home left

---
+++ src/components/CardDetails/CardDetails.js
@@ -0,0 +1,38 @@
+import React , {useState} from 'react';
+import './cardDetails.scss';
+import Url from '../Url/Url';
+import PropTypes from 'prop-types'; // ES6
+
+
+
+function CardDetails({Employer, careerUrl, Job_Title, Job_Start_Date,setFav,Specialization,
+   fav,Id,deleteRecord,University_Name}) 
+   {
+        return (
+            <div>
+            <Url
+            careerUrl={careerUrl}
+            mystyle="new-style"
+            >
+            Click Here 
+            </Url>    
+
+            <div> {Employer}</div>   
+            <div>{Job_Title}</div>
+            <div>{Job_Start_Date}</div>
+            <div>{Specialization}</div>
+            <div>{University_Name}</div>
+            <div>{Job_Title}</div>
+            
+            </div>
+            )
+  }
+
+export default CardDetails
+ 
+CardDetails.propTypes = {
+Employer:PropTypes.string.isRequired,
+careerUrl:PropTypes.string,
+Job_Title:PropTypes.string,
+
+}
---
+++ src/components/Url/Url.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Url.scss';
+import PropTypes from 'prop-types'; // ES6
+
+
+function Url({careerUrl,children:mychild,mystyle}) {
+
+ return (
+    <div className={mystyle}>
+        <a href={careerUrl}>{`${mychild} `}</a>
+    </div>)
+}
+
+
+export default Url
+
+Url.propTypes = {
+    careerUrl:PropTypes.string.isRequired,
+    mychild:PropTypes.node,
+    mystyle:PropTypes.string,
+    
+}
+
+Url.defaultProps = {
+    careerUrl: 'https://cnn.com'
+  };
