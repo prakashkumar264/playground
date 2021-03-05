fdc0368304c9d6a6236f0049f4a80932d8fe192a
PrakashKumar <kumar.prak@northeastern.edu>
2021-03-05 17:29:56 -0500

    carousel udpate

--- src/Carousel.js
+++ src/Carousel.js
@@ -25,26 +25,25 @@ const items = [
 const CarouselPage = (props) => {
   const [activeIndex, setActiveIndex] = useState(0);
 
-
   const next = () => {
+    if(activeIndex===items.length-1){
+        return
+    }
     const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
     setActiveIndex(nextIndex);
   }
 
   const previous = () => {
+    if(activeIndex===0){
+        return
+    }
     const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
     setActiveIndex(nextIndex);
   }
 
-  const goToIndex = (newIndex) => {
-    setActiveIndex(newIndex);
-  }
-
   const slides = items.map((item) => {
     return (
-      <CarouselItem
-        key={item.src}
-      >
+      <CarouselItem key={item.src} >
         <img src={item.src}  style={{width:"100%"}}/>
         <CarouselCaption  captionHeader={item.caption} />
       </CarouselItem>
@@ -53,15 +52,15 @@ const CarouselPage = (props) => {
 
   return (
       <div>
-<div style={{width:"25%",margin:"auto"}}>
+      <div style={{width:"25%",margin:"auto"}}>
         <Carousel
             activeIndex={activeIndex}
             next={next}
             previous={previous}
-            >
-      <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={goToIndex} />
+        >
+     
       {slides}
-      <CarouselControl direction="prev" directionText="Previous" onClickHandler={previous} />
+      <CarouselControl direction="prev" directionText="Previous" onClickHandler={previous}  />
       <CarouselControl direction="next" directionText="Next" onClickHandler={next} />
     </Carousel>
     </div>
